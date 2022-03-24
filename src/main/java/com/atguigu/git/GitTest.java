9288ea67239c7a54483c1f5277d3ae86277cbfed
Cgs <Cgs@atguigu.com>
2022-03-24 10:21:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @param
+ * @author Cao
+ * @description
+ * @return
+ * @date 2022/3/24-10:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+
+    }
+}
